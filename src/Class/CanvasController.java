cf4d6d5466b660760f402da5098cfb7391469006
dani0105 <danielrojas0105@gmail.com>
2019-11-04 17:19:56 -0600

    Canvas Creado

--- src/Class/CanvasController.java
+++ src/Class/CanvasController.java
@@ -6,10 +6,64 @@
 
 package Class;
 
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Path2D;
+
 /**
  *
  * @author DanielR
  */
-public class CanvasController {
+public class CanvasController extends Canvas implements ActionListener,MouseListener{
+
+    private Color color;
+    private Path2D shape;
+    
+    public CanvasController(int width,int height) {
+        this.setBackground(color.white);
+        this.setSize(width, height);
+      
+    }
+    
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D drawer = (Graphics2D) g;
+        
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
     
 }
