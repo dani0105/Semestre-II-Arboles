3e75ba2ff910fbdaccf3264f3e07824528631f49
EduardoBinns <57016116+EduardoBinns@users.noreply.github.com>
2019-11-20 14:23:44 -0600

    Ruta

--- src/Class/Singleton.java
+++ src/Class/Singleton.java
@@ -40,13 +40,13 @@ public class Singleton {
         this.map.add("Venecia", 350, 150);
 
         //                                      Peso-KM-KM/H-Time 
-        this.map.add("Santa Clara", "Florencia", 20, 8, 60, 2.5F);
+        this.map.add("Florencia", "Santa Clara", 20, 8, 60, 2.5F);
         this.map.add("Florencia", "Ciudad Quesada", 20, 10, 40, 3f);
         this.map.add("Florencia", "Pital", 30, 60, 70, 4F);
         this.map.add("Ciudad Quesada", "Aguas Zarcas", 20, 24, 40, 2.5F);
         this.map.add("Aguas Zarcas", "Pital", 20, 20, 70, 1.5F);
         this.map.add("Aguas Zarcas", "Venecia", 24, 24, 65, 2.5F);
-        this.map.add("Pital", "Venecia", 24, 24, 65, 2.5F);
+        
 
         this.users.put(new User(1, "Daniel", Licenses.B1));
         this.users.put(new User(14, "Andres", Licenses.B2));
